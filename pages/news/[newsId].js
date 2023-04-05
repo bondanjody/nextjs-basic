e3b28905a4e9b26cfa06625d7928e5f1613a90b8
bondanjody <bondanjodys@gmail.com>
2023-04-05 15:19:13 +0700

    Melakukan extraksi nilai dari dynamic parameter dengan menggunakan useRouter hook.

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,13 @@
 // ourdomain.com/news/detailpage
+
+import { useRouter } from "next/router";
+
 function DetailPage() {
-  return <h1>Detail Page</h1>;
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  return <h1>Halaman {newsId}</h1>;
 }
 
 export default DetailPage;
