a2bf305e74de823839defb0f61157698b7d1b7f7
bondanjody <bondanjodys@gmail.com>
2023-04-05 15:30:30 +0700

    Menambahkan Link pada news/index.js

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,21 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 // ourdomain.com/news
 function NewsPage() {
-  return <h1>NewsPage</h1>;
+  return (
+    <Fragment>
+      <h1>NewsPage</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-tutorial">NextJS Tutorial</Link>
+        </li>
+        <li>
+          <Link href="/news/node-tutorial">NodeJS Tutorial</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
